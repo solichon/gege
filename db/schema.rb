35f94aba42fa453e39c3a12ba2281a6d81bc6788
Samuel Olichon <samuel.olichon@gmail.com>
2018-03-21 22:33:25 +0100

    update recall button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319153338) do
+ActiveRecord::Schema.define(version: 20180321172304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,12 @@ ActiveRecord::Schema.define(version: 20180319153338) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscribers", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
