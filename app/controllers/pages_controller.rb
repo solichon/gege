6bb4d921c0f45328da2e425a87e5d16fcfd90187
Samuel Olichon <samuel.olichon@gmail.com>
2018-03-21 11:15:57 +0100

    navbar updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
+# April action: Only user test can have access to Gege sol
   def home
     if current_user
       if current_user.test == true || current_user.admin == true
         redirect_to timeslots_path
       else
-        redirect_to about_path
+        redirect_to activities_path
       end
     end
   end
