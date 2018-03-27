5294741bc37594c50dd7314816e19bf3c62453e8
Samuel Olichon <samuel.olichon@gmail.com>
2018-03-27 17:24:55 +0200

    pundit finalized & sign_up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321172304) do
+ActiveRecord::Schema.define(version: 20180327150747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,12 +64,6 @@ ActiveRecord::Schema.define(version: 20180321172304) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "subscribers", force: :cascade do |t|
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
