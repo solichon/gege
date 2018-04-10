a601250ffab9129f17475cb512e0da83f58e3a0c
Samuel Olichon <samuel.olichon@gmail.com>
2018-04-10 12:18:03 +0200

    edit activities

--- app/policies/activity_policy.rb
+++ app/policies/activity_policy.rb
@@ -19,11 +19,11 @@ class ActivityPolicy < ApplicationPolicy
 
   end
 
-  # def update?
-  # record.user == user.test || user.admin
-  # # - record: the timeslot passed to the `authorize` method in controller
-  # # - user:   the `current_user` signed in with Devise.
-  # end
+  def update?
+    record.user == user.test || user.admin
+  # - record: the timeslot passed to the `authorize` method in controller
+  # - user:   the `current_user` signed in with Devise.
+  end
 
   # def destroy?
   #   record.user == user.test || user.admin
